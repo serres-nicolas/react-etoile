8a25e22b5b87b303789cbdd09f5eeb184f6a2021
Nicolas Serres <miracle@macbook-pro-de-nicolas.home>
2022-10-19 19:18:02 +0200

    try first page finish

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,4 +1,8 @@
 import logo from "./logo.svg";
+import ellipse1 from "./ellipse1.svg";
+import ellipse2 from "./ellipse2.svg";
+import ellipse3 from "./ellipse3.svg";
+import vector from "./vector.svg";
 
 export default function Accueil() {
   return (
@@ -17,15 +21,17 @@ export default function Accueil() {
           <p>coordination des mouvements.</p>
         </div>
         <div className="boxdl">
-          <a className="dl" href="">
+          <img src={vector} id="two" alt="book logo" class="vector" />
+          <a className="dl" id="first" href="">
             Actualités
           </a>
         </div>
-        <div className="filter"></div>
+        <img src={ellipse2} id="two" alt="Gym logo" class="filtersvg2" />
       </div>
       <div class="logo">
+        <img src={ellipse3} alt="Gym logo" class="filtersvg3" />
         <img src={logo} alt="Gym logo" class="logo_svg" />
-        <div className="filter2"></div>
+        <img src={ellipse1} alt="Gym logo" class="filtersvg1" />
       </div>
     </div>
   );
--- src/pages/Leclub.js
+++ src/pages/Leclub.js
@@ -1,7 +1,9 @@
 export default function Resultat() {
   return (
-    <div className="home">
-      <h1>This page is under development</h1>
+    <div className="Flex_page">
+      <div className="home">
+        <h1>This page is under development</h1>
+      </div>
     </div>
   );
 }
